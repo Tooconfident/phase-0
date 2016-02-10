d86a4e7665c5d695a7cacb5cc9ee0b0adc2fb4e3
Ryan Wilkins <wilkins.ryan@gmail.com>
2016-02-09 16:52:20 -0800

    Add defining-variables.rb to week-4

--- week-4/defining-variables.rb
+++ week-4/defining-variables.rb
@@ -1,14 +1,14 @@
 #Solution Below
 
-
-
-
-# RSpec Tests. They are included in this file because the local variables you are creating are not accessible across files. If we try to run these files as a separate file per normal operation, the local variable checks will return nil.
 first_name = 'Ryan'
 
 last_name = 'Wilkins'
 
 age = 31
+
+
+
+# RSpec Tests. They are included in this file because the local variables you are creating are not accessible across files. If we try to run these files as a separate file per normal operation, the local variable checks will return nil.
 
 describe 'first_name' do
   it "is defined as a local variable" do
