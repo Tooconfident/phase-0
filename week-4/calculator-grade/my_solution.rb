d9d92bd34093a0ec5ad74ce638a39fbc115b014d
Ryan Wilkins <wilkins.ryan@gmail.com>
2016-02-11 22:25:59 -0800

    Add ruby files and directory for 4.5.2

---
+++ week-4/calculator-grade/my_solution.rb
@@ -0,0 +1,33 @@
+# Calculate a Grade
+
+# I worked on this challenge with: Erica Lloyd.
+
+
+# Your Solution Below
+
+def get_grade (x) #return letter grade as a string
+
+
+  if x >= 90
+
+    p "A"  
+
+  elsif x >= 80
+
+    p "B"
+
+  elsif x >= 70
+    p "C"
+
+  elsif x >= 60
+
+    p "D"
+
+  else 
+
+    p "F" 
+
+end
+end
+
+get_grade(38)
