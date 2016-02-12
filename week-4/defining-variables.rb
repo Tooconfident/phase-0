204a81303f3fff838987e170f1e874a405bcc338
Ryan Wilkins <wilkins.ryan@gmail.com>
2016-02-11 23:27:09 -0800

    Complete assignment 4.2.1

--- week-4/defining-variables.rb
+++ week-4/defining-variables.rb
@@ -6,7 +6,11 @@ last_name = 'Wilkins'
 
 age = 31
 
+# p first_name
 
+# p last_name
+
+# p age
 
 # RSpec Tests. They are included in this file because the local variables you are creating are not accessible across files. If we try to run these files as a separate file per normal operation, the local variable checks will return nil.
 
