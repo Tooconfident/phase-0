90fbee2b026fc72aeff88a1f3cbc8fcfe294cd43
Ryan Wilkins <wilkins.ryan@gmail.com>
2016-02-28 11:47:58 -0800

    Finish reflection and adjust refactor for 6.6

--- week-6/bingo_solution.rb
+++ week-6/bingo_solution.rb
@@ -83,7 +83,7 @@ class BingoBoard
   end
 
   def display
-    puts "--B---I---N---G---O--"
+    puts "-B---I---N---G---O-"
     @bingo_board.each { |array| p array }
   end
 end
@@ -98,26 +98,33 @@ board = [[47, 44, 71, 8, 88],
          [75, 70, 54, 80, 83]]
 
 new_game = BingoBoard.new(board)
-p new_game.call
+puts new_game.call
 new_game.check
-p new_game.display
-
-# Refactored Solution
-
-
-
-#DRIVER CODE (I.E. METHOD CALLS) GO BELOW THIS LINE
-
-board = [[47, 44, 71, 8, 88],
-        [22, 69, 75, 65, 73],
-        [83, 85, 97, 89, 57],
-        [25, 31, 96, 68, 51],
-        [75, 70, 54, 80, 83]]
-
-new_game = BingoBoard.new(board)
-new_game.call
-new_game.check
+new_game.display
+puts
 
 
 #Reflection
 
+# How difficult was pseudocoding this challenge? What do you think of your pseudocoding style?
+## This was one of the hardest challenges we have had so far. My pseudocode style makes sense and I believe I translated it well to code.
+## The only thing I am still confused on is printing a column to the console.
+
+# What are the benefits of using a class for this challenge?
+## All the instance methods will work together to creat a working and (somewhat) pretty Bingo board.
+
+# How can you access coordinates in a nested array?
+## By calling the array name followed by multiple index keys.
+
+# What methods did you use to access and modify the array?
+## .each, .map, and .with_index. I used .slice! in my initial solution, but that proved not as readable or logical as just the simple .each method.
+
+# Give an example of a new method you learned while reviewing the Ruby docs. Based on what you see in the docs, what purpose does it serve, and how is it called?
+## _with_index for both .each and .map was very useful in allowing me to access the index number and value of each element in an array. 
+
+# How did you determine what should be an instance variable versus a local variable?
+## I stuck to instance variables as much as possible because I knew I wanted each instance to communicate with each other without calling another method inside.
+
+# What do you feel is most improved in your refactored solution?
+## Readability and DRYness. I had a @combo instance variable in my first initialize method because I thought I would need to call on it in the future.
+## Turned out that wasn't necessary.
